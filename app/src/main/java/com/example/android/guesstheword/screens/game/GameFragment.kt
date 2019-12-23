03fac3fa67d34030e25edad8c6f0e68e6bf72283
theethawat <5910110150@psu.ac.th>
2019-12-23 17:07:20 +0700

    Add Observer

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -24,6 +24,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment
 import com.example.android.guesstheword.R
@@ -51,15 +52,23 @@ class GameFragment : Fragment() {
                 false
         )
 
+        // Get ViewModel From GameViewModel
         Log.i("GameFragment","Called ViewModelProviders.of")
         viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
+        //Observer LiveData Call Back
+        viewModel.score.observe(this, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+
+        viewModel.word.observe(this, Observer { newWord ->
+            binding.wordText.text = newWord
+         })
 
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
         binding.endGameButton.setOnClickListener { onEngGame() }
-        updateScoreText()
-        updateWordText()
+
         return binding.root
 
     }
@@ -72,30 +81,24 @@ class GameFragment : Fragment() {
 
     private fun onSkip() {
       viewModel.onSkip()
-        updateScoreText()
-        updateWordText()
+
     }
 
     private fun onCorrect() {
         viewModel.onCorrect()
-        updateWordText()
-        updateScoreText()
+
     }
 
     /** Methods for updating the UI **/
 
-    private fun updateWordText() {
-        binding.wordText.text = viewModel.word
-    }
 
-    private fun updateScoreText() {
-        binding.scoreText.text = viewModel.score.toString()
-    }
+
+
 
     private fun onEngGame(){
         Toast.makeText(activity,"Game just Finished",Toast.LENGTH_SHORT).show()
         val action = GameFragmentDirections.actionGameToScore()
-        action.score = viewModel.score
+        action.score = viewModel.score.value?:0
         NavHostFragment.findNavController(this).navigate(action)
     }
 
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,20 +1,24 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel :ViewModel(){
     // The current word
-    var word = ""
+    var word = MutableLiveData<String>()
 
     // The current score
-     var score = 0
+     var score = MutableLiveData<Int>()
 
     // The list of words - the front of the list is the next word to guess
      private lateinit var wordList: MutableList<String>
 
     init {
         Log.i("GameViewModel","Game View Model Created!")
+        //Use setValue() to set the value to LiveData but in Kotlin can use by .value = ....
+        word.value = ""
+        score.value = 0
         resetList()
         nextWord()
     }
@@ -49,21 +53,23 @@ class GameViewModel :ViewModel(){
      */
     private fun nextWord() {
         if (!wordList.isEmpty()) {
-            //Select and remove a word from the list
-            word = wordList.removeAt(0)
+            //Select and remove a word from the list change word to the next index
+            word.value = wordList.removeAt(0)
         }
     }
 
      fun onSkip() {
         if (!wordList.isEmpty()) {
-            score--
+          //Instead of  score--
+            score.value = (score.value)?.minus(1)
         }
         nextWord()
     }
 
     fun onCorrect() {
         if (!wordList.isEmpty()) {
-            score++
+            //Instead of score++
+            score.value = (score.value)?.plus(1)
         }
         nextWord()
     }
